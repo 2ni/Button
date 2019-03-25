e6259ebb430bcadc7e4819adc4e7420e0e569880
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2019-03-25 18:16:35 +0100

    fix some missing code in the example

--- examples/simple_example.cpp
+++ examples/simple_example.cpp
@@ -5,9 +5,16 @@
  */
 
 #include <Arduino.h>
-
 #include <Button.h>
 
+#define BUTTON 39 // any GPIO pin
+#define LED 17    // GPIO where a LED is connected to
+
+/*
+ * Callback function when a push is detected
+ * <type> can be SHORT_PUSH or LONG_PUSH
+ *
+ */
 void buttonPressed(Button::pushType type) {
   if (type == Button::SHORT_PUSH) {
     Serial.println("short push");
@@ -15,6 +22,8 @@ void buttonPressed(Button::pushType type) {
     Serial.println("long push");
   }
 }
+
+Button button = Button(BUTTON, LED, buttonPressed);
 
 void setup() {
   // uart
